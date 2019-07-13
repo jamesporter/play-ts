61f9431fa389e0a4558dac53ae5e0a5863bd8e97
James Porter <james@amimetic.co.uk>
2019-07-13 18:25:03 +0100

    this seems to do nicely... sensible/human friendly text api
    pick either width or height and figure out rest, centre vertically w.r.t. coords, horizontal alignment is choice (could do same for vertical but no need right now?)

--- src/lib/path.ts
+++ src/lib/path.ts
@@ -6,6 +6,10 @@ export interface Traceable {
   traceIn(ctx: CanvasRenderingContext2D);
 }
 
+export interface Textable {
+  textIn(ctx: CanvasRenderingContext2D);
+}
+
 export interface SVGPathable {
   svgPath: string;
 }
@@ -293,6 +297,90 @@ export class Rect implements Traceable {
             })
         );
       }
+    }
+  };
+}
+
+export type TextSizing = "fixed" | "fitted";
+export type TextHorizontalAlign = CanvasRenderingContext2D["textAlign"];
+export type FontStyle = "normal" | "italic" | "oblique";
+export type FontVariant = "normal" | "small-caps";
+export type FontWeight =
+  | "normal"
+  | "bold"
+  | "bolder"
+  | "lighter"
+  | "100"
+  | "200"
+  | "300"
+  | "400"
+  | "500"
+  | "600"
+  | "700"
+  | "800"
+  | "900";
+export enum Font {
+  Arial = "Arial",
+  Helvetica = "Helvetica",
+  TimesNewRoman = "Times New Roman",
+  Times = "Times",
+  CourierNew = "Courier New",
+  Courier = "Courier",
+  Palatino = "Palatino",
+  Garamond = "Garamond",
+  Bookman = "Bookman",
+  AvantGarde = "Avant Garde",
+  System = "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif"
+}
+export type TextConfig = {
+  sizing?: TextSizing;
+  align?: TextHorizontalAlign;
+  size: number;
+  font?: Font;
+  at: Point2D;
+  kind: "fill" | "stroke";
+  style?: FontStyle;
+  weight?: FontWeight;
+  variant?: FontVariant;
+};
+
+export class Text implements Textable {
+  /**
+   * Text is always vertically aligned
+   * By default is fixed (specified vertcial font size) but can choose fitted, then will fit horizontally to size
+   * @param config Configuration
+   */
+  constructor(private config: TextConfig, private text: string) {}
+
+  textIn = (ctx: CanvasRenderingContext2D) => {
+    const {
+      size,
+      sizing = "fixed",
+      align = "center",
+      font = Font.System,
+      at,
+      style = "normal",
+      weight = "normal",
+      variant = "normal",
+      kind
+    } = this.config;
+    ctx.textAlign = align;
+
+    let y: number;
+    if (sizing === "fixed") {
+      ctx.font = `${style} ${variant} ${weight} ${size}px ${font}`;
+      y = at[1] + size / 2;
+    } else {
+      ctx.font = `${style} ${variant} ${weight} 1px ${font}`;
+      const { width: mW } = ctx.measureText(this.text);
+      const sizeH = size / mW;
+      ctx.font = `${style} ${variant} ${weight} ${sizeH}px ${font}`;
+      y = at[1] + sizeH / 2;
+    }
+    if (kind === "fill") {
+      ctx.fillText(this.text, at[0], y);
+    } else {
+      ctx.strokeText(this.text, at[0], y);
     }
   };
 }
