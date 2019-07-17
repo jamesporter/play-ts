dfdd9865ca40d0aa7ed39e278680a6281c6cb482
James Porter <james@amimetic.co.uk>
2019-07-17 21:03:48 +0100

    mvpublishablepackage

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
-export * from "./play-canvas";
+export { default as PlayCanvas } from "./play-canvas";
+export * from "./noise";
+export * from "./path";
+export * from "./colours";
+export { default as v } from "./vectors";
+export * from "./gradient";
+export { default as c } from "./collectionOps";
+export * from "./types/play";
